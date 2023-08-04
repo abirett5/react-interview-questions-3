aaac58d836687a0f90fa498989848bef7f07ee66
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-08-04 20:45:16 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./styles.css";
-import Folder from "./components/Folder";
-import explorer from "./data/folderData";
 
 export default function App() {
+  const myDebounce = (callback, delay) => {
+    let timer;
+    return function (...args) {
+      if(timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(args);
+      }, delay);
+    };
+  };
+
+  const handleChange = myDebounce((e) => {
+    console.log(e.target.value);
+  }, 1000);
+
   return (
     <div className="App">
-      <Folder explorer={explorer} />
+      <input onChange={handleChange} />
     </div>
   );
 }
